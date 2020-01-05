89ab2025fec59557eba58d9295fb09530a3b559f
Ivan Morandi <ivanmorandi@MacBook-Pro.fritz.box>
2020-01-05 16:56:41 +0100

    Small refactoring + comments

--- test_taxes_sales_problem.py
+++ test_taxes_sales_problem.py
@@ -1,79 +1,83 @@
-from lib.orders import Orders
+from lib.order import Order
 from lib.product import Product
-from lib.producttype import ProductType
+from lib.category import Category
 from lib.taxescalculator import TaxesCalculator
-from lib.parser import Parser
 
 def test_product():
-    product = Product('test', ProductType.Goods, 10.00)
+    # Test the product class
+    product = Product('test', Category.Goods, 10.00)
 
     assert product.getName() == 'test'
-    assert product.getType() == ProductType.Goods
+    assert product.getCategory() == Category.Goods
     assert product.getPrice() == 10.00
 
-def test_product_type():
-    assert ProductType.toProductType('booksss') == ProductType.Books
-    assert ProductType.toProductType('boosss') == ProductType.Goods
-    assert ProductType.toProductType('Books') == ProductType.Books
-    assert ProductType.toProductType('BoOk') == ProductType.Books
+def test_category():
+    # Test the category parser.
+    assert Category.toCategory('booksss') == Category.Books
+    assert Category.toCategory('boosss') == Category.Goods
+    assert Category.toCategory('Books') == Category.Books
+    assert Category.toCategory('BoOk') == Category.Books
 
-    assert ProductType.toProductType('food') == ProductType.Food
-    assert ProductType.toProductType('foods') == ProductType.Food
-    assert ProductType.toProductType('FoOd') == ProductType.Food
-    assert ProductType.toProductType('ffoods') == ProductType.Food
+    assert Category.toCategory('food') == Category.Food
+    assert Category.toCategory('foods') == Category.Food
+    assert Category.toCategory('FoOd') == Category.Food
+    assert Category.toCategory('ffoods') == Category.Food
 
-    assert ProductType.toProductType('medical products') == ProductType.Medicals
-    assert ProductType.toProductType('medical') == ProductType.Medicals
-    assert ProductType.toProductType('medicals') == ProductType.Medicals
-    assert ProductType.toProductType('medicine') == ProductType.Goods
+    assert Category.toCategory('medical products') == Category.Medicals
+    assert Category.toCategory('medical') == Category.Medicals
+    assert Category.toCategory('medicals') == Category.Medicals
+    assert Category.toCategory('medicine') == Category.Goods
 
-    assert ProductType.toProductType('test') == ProductType.Goods
-    assert ProductType.toProductType('') == ProductType.Goods
-    assert ProductType.toProductType('goods') == ProductType.Goods
-    assert ProductType.toProductType('medic4ls') == ProductType.Goods
+    assert Category.toCategory('test') == Category.Goods
+    assert Category.toCategory('') == Category.Goods
+    assert Category.toCategory('goods') == Category.Goods
+    assert Category.toCategory('medic4ls') == Category.Goods
 
 def test_orders_insert():
-    orders = Orders()
+    # Test the insertion and deletion of products for an order.
+    order = Order()
 
-    orders.add(Product('book', ProductType.Books, 12.49), 2, False)
-    assert len(orders.getOrders()) == 1
+    order.add(Product('book', Category.Books, 12.49), 2, False)
+    assert len(order.getOrder()) == 1
 
-    orders.add(Product('music CD', ProductType.Goods, 14.99), 1, False)
-    assert len(orders.getOrders()) == 2
+    order.add(Product('music CD', Category.Goods, 14.99), 1, False)
+    assert len(order.getOrder()) == 2
 
-    orders.add(Product('chocolate bar', ProductType.Food, 0.85), 1, False)
-    assert len(orders.getOrders()) == 3
+    order.add(Product('chocolate bar', Category.Food, 0.85), 1, False)
+    assert len(order.getOrder()) == 3
 
-    orders.clean()
-    assert orders.getOrders() == []
-    assert len(orders.getOrders()) == 0
+    order.clean()
+    assert order.getOrder() == []
+    assert len(order.getOrder()) == 0
 
 def test_orders_price():
-    orders = Orders()
-    orders.add(Product('book', ProductType.Books, 12.49), 2, False)
-    orders.add(Product('music CD', ProductType.Goods, 14.99), 1, False)
-    orders.add(Product('chocolate bar', ProductType.Food, 0.85), 1, False)
+    # Test the price computation of the orders.
+    order = Order()
+    order.add(Product('book', Category.Books, 12.49), 2, False)
+    order.add(Product('music CD', Category.Goods, 14.99), 1, False)
+    order.add(Product('chocolate bar', Category.Food, 0.85), 1, False)
     
-    assert orders.getTaxes() == 1.50
-    assert orders.getPrice() == 42.32
+    assert order.getTaxes() == 1.50
+    assert order.getPrice() == 42.32
 
-    orders.clean()
-    orders.add(Product('box of chocolates', ProductType.Food, 10.00), 1, True)
-    orders.add(Product('bottle of perfume', ProductType.Goods, 47.50), 1, True)
+    order.clean()
+    order.add(Product('box of chocolates', Category.Food, 10.00), 1, True)
+    order.add(Product('bottle of perfume', Category.Goods, 47.50), 1, True)
     
-    assert orders.getTaxes() == 7.65
-    assert orders.getPrice() == 65.15
+    assert order.getTaxes() == 7.65
+    assert order.getPrice() == 65.15
 
-    orders.clean()
-    orders.add(Product('bottle of perfume', ProductType.Goods, 27.99), 1, True)
-    orders.add(Product('bottle of perfume', ProductType.Goods, 18.99), 1, False)
-    orders.add(Product('packet of headache pills', ProductType.Medicals, 9.75), 1, False)
-    orders.add(Product('box of chocolates', ProductType.Food, 11.25), 3, True)
+    order.clean()
+    order.add(Product('bottle of perfume', Category.Goods, 27.99), 1, True)
+    order.add(Product('bottle of perfume', Category.Goods, 18.99), 1, False)
+    order.add(Product('packet of headache pills', Category.Medicals, 9.75), 1, False)
+    order.add(Product('box of chocolates', Category.Food, 11.25), 3, True)
     
-    assert orders.getTaxes() == 7.90
-    assert orders.getPrice() == 98.38
+    assert order.getTaxes() == 7.90
+    assert order.getPrice() == 98.38
 
 def test_taxes_calculator_round():
+    # Test the round up function to the nearest 0.05
     assert TaxesCalculator.round(0.0) == 0.0
     assert TaxesCalculator.round(0.001) == 0.05
     assert TaxesCalculator.round(0.04) == 0.05
@@ -81,15 +85,17 @@ def test_taxes_calculator_round():
     assert TaxesCalculator.round(0.075) == 0.1
     assert TaxesCalculator.round(0.085) == 0.1
 
-def test_taxes_calculator_type():
-    assert TaxesCalculator.getTaxesFromProductType(Product("test", ProductType.Goods, 1.9)) == 0.1
-    assert TaxesCalculator.getTaxesFromProductType(Product("test", ProductType.Food, 1.9)) == 0
-    assert TaxesCalculator.getTaxesFromProductType(Product("test", ProductType.Books, 1.9)) == 0
-    assert TaxesCalculator.getTaxesFromProductType(Product("test", ProductType.Medicals, 1.9)) == 0
+def test_taxes_calculator_category():
+    # Test the taxes for each category
+    assert TaxesCalculator.getTaxesFromCategory(Product("test", Category.Goods, 1.9)) == 0.1
+    assert TaxesCalculator.getTaxesFromCategory(Product("test", Category.Food, 1.9)) == 0
+    assert TaxesCalculator.getTaxesFromCategory(Product("test", Category.Books, 1.9)) == 0
+    assert TaxesCalculator.getTaxesFromCategory(Product("test", Category.Medicals, 1.9)) == 0
     
 
 def test_taxes_calculator_taxes():
-    prod1 = Product('book', ProductType.Books, 27.99)
+    # Test the taxes and price computation
+    prod1 = Product('book', Category.Books, 27.99)
     assert TaxesCalculator.getImportTaxes(prod1, 1, False) == 0
     assert TaxesCalculator.getImportTaxes(prod1, 10, False) == 0
     assert TaxesCalculator.getImportTaxes(prod1, 1, True) == 1.40
@@ -108,7 +114,7 @@ def test_taxes_calculator_taxes():
     assert TaxesCalculator.getPrice(prod1, 1, True) == 29.39
     assert TaxesCalculator.getPrice(prod1, 10, True) == 293.9
 
-    prod2 = Product('bottle of perfume', ProductType.Goods, 18.99)
+    prod2 = Product('bottle of perfume', Category.Goods, 18.99)
     assert TaxesCalculator.getImportTaxes(prod2, 1, False) == 0
     assert TaxesCalculator.getImportTaxes(prod2, 10, False) == 0
     assert TaxesCalculator.getImportTaxes(prod2, 1, True) == 0.95
